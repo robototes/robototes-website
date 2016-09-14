e8430456173097b086db64cb2a3eef10ea098407
dannytech <dhellstern@outlook.com>
2016-09-14 23:42:47 +0000

    Various fixes

--- code/classes.js
+++ code/classes.js
@@ -19,5 +19,11 @@ module.exports = {
             DEVELOPMENT: 8081
         },
         domain: "c9users.io"
+    },
+    fulldomains: {
+        STATUS: function() { return module.exports.constants.subdomains.STATUS + "." + module.exports.constants.domain + ":" + module.exports.constants.ports[process.env.NODE_ENV.toUpperCase()] },
+        PUBLIC: function() { return module.exports.constants.subdomains.PUBLIC + "." + module.exports.constants.domain + ":" + module.exports.constants.ports[process.env.NODE_ENV.toUpperCase()] },
+        CDN: function() { return module.exports.constants.subdomains.CDN + "." + module.exports.constants.domain + ":" + module.exports.constants.ports[process.env.NODE_ENV.toUpperCase()] },
+        NONE: function() { return module.exports.constants.subdomains.NONE + "." + module.exports.constants.domain + ":" + module.exports.constants.ports[process.env.NODE_ENV.toUpperCase()] }
     }
 };

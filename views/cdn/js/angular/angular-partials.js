e34226c1f34bfe41fb4aed6abf0453b69265fc9d
dannytech <dhellstern@outlook.com>
2016-10-09 20:45:45 +0000

    General commit

--- views/cdn/js/angular/angular-partials.js
+++ views/cdn/js/angular/angular-partials.js
@@ -6,20 +6,19 @@ This file is part of the robototes-website project.
 Any copying and/or distributing and/or use in commercial or non-commercial environments
 via any medium without the express permission of Robotics Leadership is strictly prohibited
  */
-var pagesApp = angular.module("robototes-pages", [
+var partialsApp = angular.module("robototes-partials", [
         "robototes-common"
     ]);
-pagesApp.controller("homeController", [
+partialsApp.controller("footerController", [
     "$scope",
     "$controller",
     function($scope, $controller) {
         angular.extend(this, $controller("commonController", { $scope: $scope }));
-    }
-]);
-pagesApp.controller("errorController", [
-    "$scope",
-    "$controller",
-    function($scope, $controller) {
-        angular.extend(this, $controller("commonController", { $scope: $scope }));
+        
+        // Google analytics opt-out
+        $scope.optout = function() {
+            console.log("u");
+            window["ga-disable-" + ga.q[0][1]] = true;
+        };
     }
 ]);

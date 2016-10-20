8f0d9a08a5b54e7b39a31ee00e81da5f6c9a5eb8
dannytech <dhellstern@outlook.com>
2016-10-20 03:15:56 +0000

    Fixed bug in asynchronous testing

--- code/tests/module_tests.js
+++ code/tests/module_tests.js
@@ -7,6 +7,7 @@ Any copying and/or distributing and/or use in commercial or non-commercial envir
 via any medium without the express permission of Robotics Leadership is strictly prohibited
  */
 var expect = require("chai").expect,
+    async = require("async"),
     request = require("supertest"),
     extend = require("util")._extend;
 
@@ -14,20 +15,19 @@ describe("Modules", function() {
     var package = require("../../package"),
         modules = extend(package.dependencies, package.devDependencies);
     
-    for(var i in modules) {
-        if(modules.hasOwnProperty(i))
-            describe(i, function() {
-                it("is installed", function() {
-                    expect(require(i)).to.not.be.equal(null);
-                });
-                it("is up to date", function(done) {
-                    this.timeout(10000);
-                    require("child_process").exec("npm show " + i + " version", function(err, stdout, stderr) {
-                        if(err) return done(err);
-                        expect(stdout.trim()).to.be.equal(modules[i].substr(1));
-                        done();
-                    });
+    async.each(Object.keys(modules), function(current, callback) {
+        describe(current, function() {
+            it("is installed", function() {
+                expect(require(current)).to.not.be.equal(null);
+            });
+            it("is up to date", function(done) {
+                this.timeout(10000);
+                require("child_process").exec("npm show " + current + " version", function(err, stdout, stderr) {
+                    if(err) return done(err);
+                    expect(stdout.trim()).to.be.equal(modules[current].substr(1));
+                    done();
                 });
             });
-    }
+        });
+    });
 });

98fdaaeaf94f24eecf2bfebbaa9d394607072a21
te36 <timothyeng36@gmail.com>
2016-11-09 01:35:08 +0000

    Removed Angular because it was hurting our brains :(

--- views/cdn/js/tests/client_tests.js
+++ views/cdn/js/tests/client_tests.js
@@ -19,49 +19,4 @@ describe("Dependencies", function() {
             expect(jQuery.fn.jquery).to.be.equal("3.1.0");
         });
     });
-    describe("AngularJS", function() {
-        it("should be loaded", function() {
-            expect(angular).to.not.be.null;
-        });
-        // robototes-common app
-        describe("robototes-common", function() {
-            var $scope = {}, controller;
-            beforeEach(module("robototes-common"));
-            describe("commonController", function() {
-                beforeEach(inject(function($controller) {
-                    controller = $controller("commonController", { $scope: $scope });
-                }));
-            });
-        });
-        // robototes-pages app
-        describe("robototes-pages", function() {
-            beforeEach(module("robototes-pages"));
-            describe("homeController", function() {
-                var $scope = {}, controller;
-                beforeEach(inject(function($controller) {
-                    controller = $controller("homeController", { $scope: $scope });
-                }));
-            });
-            describe("errorController", function() {
-                var $scope = {}, controller;
-                beforeEach(inject(function($controller) {
-                    controller = $controller("errorController", { $scope: $scope });
-                }));
-            });
-        });
-        // robototes-dev app
-        describe("robototes-dev", function() {
-            beforeEach(module("robototes-dev"));
-            describe("devController", function() {
-                var $scope = {}, controller;
-                beforeEach(inject(function($controller) {
-                    controller = $controller("devController", { $scope: $scope });
-                }));
-                it("should have runTests function", function() {
-                    expect($scope.runTests).to.not.be.null;
-                    expect(typeof $scope.runTests).to.be.equal("function");
-                });
-            });
-        });
-    });
 });

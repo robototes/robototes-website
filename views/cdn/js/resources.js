f3a1677ab5c8c70ee9ff8017728d4cd370c7ea64
te36 <timothyeng36@gmail.com>
2016-11-10 01:06:55 +0000

    Added website emails.

--- views/cdn/js/resources.js
+++ views/cdn/js/resources.js
@@ -17,15 +17,27 @@ $(function() {
         "maneaa",
         "engt"
     ];
-    for(var i = 0; i < leadership_names.length; i++) {
-        var link = $('<div class="col-sm-6 col-xs-12"><a style="cursor: pointer" class="contactlink" id="lcontact_' + i + '">' + leadership_names[i] + '</a></div>');
-        $("#leadership_emails").append(link);
+    var website_names = [
+        "Daniel Hellstern",
+        "Timothy Eng"
+    ];
+    var website_emails = [
+        "hellsternd",
+        "engt"
+    ];
+    function genLinks(namearr, emailarr, linkname, divname) {
+        for(var i = 0; i < namearr.length; i++) {
+            var link = $('<div class="col-sm-6 col-xs-12"><a style="cursor: pointer" class="contactlink" id="' + linkname + 'contact_' + i + '">' + namearr[i] + '</a></div>');
+            $("#" + divname).append(link);
+        }
+        $(".contactlink").mouseover(function() {
+            var index = parseInt($(this).attr("id").split("_")[1], 10);
+            $(this).attr("href", "mailto:s-" + emailarr[index] + "@bsd405.org");
+        });
+        $(".contactlink").mouseleave(function() {
+            $(this).attr("href", "");
+        });
     }
-    $(".contactlink").mouseover(function() {
-        var index = parseInt($(this).attr("id").split("_")[1], 10);
-        $(this).attr("href", "mailto:s-" + leadership_emails[index] + "@bsd405.org");
-    });
-    $(".contactlink").mouseleave(function() {
-        $(this).attr("href", "");
-    });
+    genLinks(leadership_names, leadership_emails, "l",  "leadership_emails");
+    genLinks(website_names, website_emails, "w",  "website_emails");
 });

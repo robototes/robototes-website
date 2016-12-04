fb5f775efe30354a1caf2e962bed54e1c38af30d
dhellstern <dhellstern@outlook.com>
2016-12-04 08:55:24 +0000

    Updated Mocha implementation to fix tests only being able to be run once, then all others error

--- views/cdn/js/tests/client_tests.js
+++ views/cdn/js/tests/client_tests.js
@@ -6,24 +6,31 @@ This file is part of the robototes-website project.
 Any copying and/or distributing and/or use in commercial or non-commercial environments
 via any medium without the express permission of Robotics Leadership is strictly prohibited
  */
-$(document).ready(function() {
-    $("[data-action=run-tests]").click(function() {
-        $("#mocha").empty();
-        mocha.run();
-    });
-});
 
 // External libraries
 var expect = chai.expect;
 
-// Checks for required libraries
-describe("Dependencies", function() {
-    describe("jQuery", function() {
-        it("should be loaded", function() {
-            expect(jQuery).to.not.be.null;
+$(document).ready(function() {
+    $("[data-action=run-tests]").click(function() {
+        $("#mocha").empty(); // Clears previous tests
+        
+        mocha = new Mocha;
+        mocha.reporter("html"); // Will output tests to #mocha div
+        
+        mocha.suite.emit('pre-require', window, null, mocha); // Prepares for tests
+        
+        // Checks for required libraries
+        describe("Dependencies", function() {
+            describe("jQuery", function() {
+                it("should be loaded", function() {
+                    expect(jQuery).to.not.be.null;
+                });
+                it("should be the latest version", function() {
+                    expect(jQuery.fn.jquery).to.be.equal("3.1.0");
+                });
+            });
         });
-        it("should be the latest version", function() {
-            expect(jQuery.fn.jquery).to.be.equal("3.1.0");
-        });
+        
+        mocha.run();
     });
 });

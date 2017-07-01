311d672f0faf1824c294322d31b054cc8997f849
dhellstern <dhellstern@outlook.com>
2017-07-01 10:56:01 +0000

    Added configs system

---
+++ configs/sponsors.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    website: 'https://www.microsoft.com/',
+    image: {
+      filename: 'microsoft.png',
+      alt: 'Microsoft'
+    }
+  },
+  {
+    website: 'https://www.t-mobile.com/',
+    image: {
+      filename: 'tmobile.png',
+      alt: 'T-Mobile'
+    }
+  },
+  {
+    website: 'http://www.speea.org/',
+    image: {
+      filename: 'speea.png',
+      alt: 'SPEEA'
+    }
+  },
+  {
+    website: 'http://www.k12.wa.us/',
+    image: {
+      filename: 'ospi.png',
+      alt: 'Office of Superintendent of Public Instruction'
+    }
+  },
+  {
+    website: 'http://www.bellevueschoolsfoundation.org/SitePages/Default.aspx',
+    image: {
+      filename: 'bsfoundation.png',
+      alt: 'Bellevue Schools Foundation'
+    }
+  },
+  {
+    website: 'http://www.intellectualventures.com/',
+    image: {
+      filename: 'intellectualventures.png',
+      alt: 'Intellectual Ventures'
+    }
+  },
+  {
+    website: 'https://f5.com/',
+    image: {
+      filename: 'f5_new.png',
+      alt: 'F5 Networks'
+    }
+  }
+]

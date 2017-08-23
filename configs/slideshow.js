a605480afa24a5105da5327be1672c9aa72db717
dhellstern <dhellstern@outlook.com>
2017-08-23 22:16:57 +0000

    Added slideshow config that was missed by last commit

---
+++ configs/slideshow.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    filename: 'falca.jpg',
+    title: 'Falca, our 2017 robot, competed at Glacier Peak and Auburn and won several awards'
+  },
+  {
+    filename: 'falca_climb.jpg',
+    title: 'Falca is able to lift its entire weight using a chain-driven climber'
+  },
+  {
+    filename: 'falca_gear.jpg',
+    title: 'Falca uses a pneumatic gripper to pick gears up off the ground'
+  }
+]

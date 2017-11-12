f1134071aac8df5b554e289b518b5c7ee4a98d50
dhellstern <dhellstern@outlook.com>
2017-11-12 05:30:52 +0000

    Removed dnsconfig

--- semaphore_commands.sh
+++ semaphore_commands.sh
@@ -25,21 +25,18 @@ docker push robototes/robototes-website-$NAME
 #### For this repository
 
 ## Build stages
-# Preview DNS changes
-docker run --rm -v $(pwd)/dnsconfig.js:/dns/dnsconfig.js \
-  -v $(pwd)/creds.json.env:/dns/creds.json.env \
-  -e "CF_API_USER=$CF_API_USER" \
-  -e "CF_API_KEY=$CF_API_KEY" \
-  stackexchange/dnscontrol dnscontrol preview
+# Download CDN files
+svn export https://github.com/robototes/robototes-website-web/trunk/views/cdn/
+# List CDN files
+ls -l $(pwd)/cdn/
 
 ## Deployments
 
 # Production
 # Upload CDN files to S3
-git clone https://github.com/robototes/robototes-website-web.git web/
 docker run --rm -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
   -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY " \
-  -v $(pwd)/web/views/cdn/:/workspace/ \
+  -v $(pwd)/cdn/:/workspace/ \
   xueshanf/awscli s3 sync / s3://cdn.robototes.com/
 # Purge the Cloudflare cache of our CDN files
 docker run --rm -e "CF_ZONE_ID=$CF_ZONE_ID" \
@@ -53,10 +50,3 @@ docker run --rm -v $(pwd)/docker-compose.yml:/workspace/docker-compose.yml \
   -e "RANCHER_ACCESS_KEY=$RANCHER_ACCESS_KEY" \
   -e "RANCHER_SECRET_KEY=$RANCHER_SECRET_KEY" \
   monostream/rancher-compose up --upgrade --pull --confirm-upgrade
-sed -i "s/{ SERVER_IP }/$SERVER_IP/g" dnsconfig.js # Fill in server IP and CDN URL
-# Update our DNS
-docker run --rm -v $(pwd)/dnsconfig.js:/dns/dnsconfig.js \
-  -v $(pwd)/creds.json.env:/dns/creds.json.env \
-  -e "CF_API_USER=$CF_API_USER" \
-  -e "CF_API_KEY=$CF_API_KEY" \
-  stackexchange/dnscontrol dnscontrol push
